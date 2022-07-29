09ff9ead425cbeaa4aa4bf6c56bd5a6ea9431e19
Jakub Igla <jakub.igla@getindata.com>
2022-07-29 11:46:19 +0200

    Add explanaition for the tflint exception

--- versions.tf
+++ versions.tf
@@ -1,6 +1,8 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
+    # The reason for the exception below is github action failing the check for unknown reason.
+    # Locally executed linter does not fail for the same dependencies
     # tflint-ignore: terraform_unused_required_providers
     azurerm = {
       source  = "hashicorp/azurerm"
