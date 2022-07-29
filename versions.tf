76c2af6b2682b5c14cc4d8ac979103110763d19d
Jakub Igla <jakub.igla@getindata.com>
2022-07-29 11:37:11 +0200

    Ignore tflint for terraform_unused_required_providers

--- versions.tf
+++ versions.tf
@@ -1,6 +1,7 @@
 terraform {
   required_version = ">= 0.13"
   required_providers {
+    # tflint-ignore: terraform_unused_required_providers
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.0"
